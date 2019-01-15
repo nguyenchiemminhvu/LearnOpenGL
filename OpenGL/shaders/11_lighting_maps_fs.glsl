104b52c40ba995f56f0fb9c214d3d51da96d3819
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2019-01-15 11:51:16 +0700

    kj

--- OpenGL/shaders/11_lighting_maps_fs.glsl
+++ OpenGL/shaders/11_lighting_maps_fs.glsl
@@ -27,19 +27,18 @@ void main()
 	vec4 FinalColor = vec4(0.0);
 
 	vec3 ambient;
-	float ambientStrength = 0.1;
-	ambient = ambientStrength * lightColor * texture2D(object.diffuse, vUV).rgb;
+	ambient = lightColor * texture2D(object.diffuse, vUV).rgb;
 
 	vec3 diffuse;
 	vec3 lightDir = normalize(vPos - lightPos);
 	float diff = max(dot(-lightDir, vNormal), 0.0);
-	diffuse = diff * lightColor * texture2D(object.diffuse, vUV).rgb;
+	diffuse = lightColor * diff * texture2D(object.diffuse, vUV).rgb;
 
 	vec3 specular;
 	vec3 fragToEye = normalize(eyePos - vPos);
 	vec3 reflectDir = reflect(lightDir, vNormal);
 	float spec = pow(max(dot(reflectDir, fragToEye), 0.0), 128.0);
-	specular = spec * lightColor * object.specular;
+	specular = lightColor * spec * object.specular;
 
 	FinalColor = vec4(ambient + diffuse + specular, 1.0);
 	gl_FragColor = FinalColor;
